7236688071bd7cf4348a9744bb347e4186e09a50
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-29 09:48:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+#  1mon = 7 
+
+# 2tues = (7+6)%2 = 1 
+# 3tues = tues + 3 = 4 
+# 4wed = mon % 4 = 3 
+# 5thu = mon * 2 +5 = 19
+# 6wed = mon +2 * tues = 15
+# 7tues = mon * wed = 
+
+
+# print(15*7)
+# print("This is a program with errors')
+
+# small_val -> min(numbers)
+# small_val = 0
+
+# print("The value returned is {}". substitute(small_val))
+
+# divisible by both 5 and 3 , means x%5 = 0 amd X%3 =0
+
+# for i in range (150): 
+  
+#   for j in range(30):
+#     calc = i * j
+#     # if calc%10 ==0: 
+#     # print(i)
+#     print (j)
+      
+# def fun(t, e, i):
+#   ret = t[:i]
+#   ret.append(e)
+#   return ret + t[i:]
+
+# lst = [8, 7, 3, 15, -4, 'foo', 3, 15, -4]
+# print(lst.length)
+
+# words = [[],[],[],[]]
+# print(len(words))
+# print(words.length)
+
+
+weather = {"main": {"temp": "66.16"}}
+
+
+# print(weather.get("main").get("temp)"))
+
+# print(weather.get("main", "temp"))
+print(weather.access("main", "temp"))
